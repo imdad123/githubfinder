09e901e153cb714129c30aeeb12c5335082e387b
unknown <uimdad19@gmail.com>
2021-03-04 15:18:20 +0500

    class 4

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -7,27 +7,45 @@ import React,{ Component, Fragment } from 'react';
 import { Users } from "./components/users/Users";
 import { Search  } from "./components/users/Search";
 import axios from 'axios';
-
+import  Alert  from "./components/layouts/Alert";
 class App extends Component{
  /*componentDidMount(){
    axios.get('https://api.github.com/users').then(res=>console.log(res.data));
  }*/
  state ={
    user:[],
-   loading:false
+   loading:false,
+   alert:null
  };
- async componentDidMount(){
+ /*async componentDidMount(){
    this.setState({loading:true});
   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`);
  console.log(res.data);
   this.setState({user:res.data,loading:false});
+}
+*/
+searchUser= async (text)=>{
+  this.setState({loading:true});
+  const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`);
+
+  this.setState({user:res.data.items,loading:false});
+}
+setAlert=(msg,type)=>{
+  this.setState({alert:{msg,type}});
+  setTimeout(() => {
+    this.setState({alert:null});
+  }, 5000);
+}
+clearUser = ()=>{
+  this.setState({user:[],loading:false});
 }
   render()
   { 
     return (
    <div className="App">
      <Nav title="Github Finder"/>
-     <Search/>
+     <Alert alert={this.state.alert}/>
+     <Search searchUser={this.searchUser} showClear = {this.state.user.length >0 ?true :false} setAlert={this.setAlert}/>
      <Users loading={this.state.loading} users={this.state.user}/>
    </div>
     );
--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export  class Search extends Component {
     state = {
         text:''
     }
+     onchange= (e)=>{
+         this.setState({text:e.target.value});
+     }
+     static propTypes = {
+         searchUser:PropTypes.func.isRequired,
+         
+     };
+     onsubmit=(e)=>
+     { e.preventDefault();
+         if(this.state.text === '')
+         {
+             this.props.setAlert("Please enter a text",'lighter');
+         }else{        
+         this.props.searchUser(this.state.text);
+         this.setState({text:''});
+         }
+     }
     render() {
         return (
             <div>
-                <form className="form">
-                    <input type="text" name="" id="" placeholder="Search User" value={this.state.text}/>
+                <form className="form" onSubmit={this.onsubmit}>
+                    <input type="text" name="" id="" placeholder="Search User" value={this.state.text} onChange={this.onchange}/>
                      <input type="submit" value="Search" className="btn btn-success"/>
-
+                     {this.props.showClear &&(
+             <button className="btn btn-success" onClick={this.props.clearUser}>Clear</button>)}
                 </form>
             </div>
         )
